767b68a42dff96b6a839712bc2bdb398dfffdc00
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-28 00:40:25 +0600

    deposit and balance update done

--- deposit.js
+++ deposit.js
@@ -1,3 +1,19 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
-    console.log('deposit button clicked');
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmount = depositField.value;
+
+    const depositElement = document.getElementById('deposit-element');
+    const previousDeposit = depositElement.innerText;
+    const depositTotal = parseInt(newDepositAmount) + parseInt(previousDeposit);
+    // update deposit 
+    depositElement.innerText = depositTotal;
+
+    // update balance 
+    const previousBalanceField = document.getElementById('balance')
+    let previousBalanceString = previousBalanceField.innerText;
+    const previousBalance = parseInt(previousBalanceString);
+    const updateBalance = previousBalance + parseInt(newDepositAmount);
+    previousBalanceField.innerText = updateBalance;
+
+    depositField.value = ''
 })
